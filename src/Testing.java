73ebda2f6d798bb51b2b8c9b35987c06ddcd0a8b
Shirley2002 <shirleymah159@gmail.com>
2020-12-28 01:59:14 +0800

    Added rowRange.java

--- src/Testing.java
+++ src/Testing.java
@@ -37,7 +37,7 @@ public class Testing {
 
         System.out.println(data.toString());*/
 
-        String filePath = "C:\\Users\\user\\Desktop\\SimpleDataFrame\\src\\Book1.csv";
+        String filePath = "C:\\Users\\User\\SimpleDataFrame\\src\\Book1.csv";
         final long startTime = System.nanoTime();
 
         //DBReader read = new DBReader("groupmemberdb");
@@ -46,19 +46,23 @@ public class Testing {
         //read.readDB();
         System.out.println(r.DATA.toString());
         Statistics s = new Statistics();
+        rowRange rowRange= new rowRange();
+        rowRange.colRange(new String[]{"name", "salary"});
+
+
         System.out.println(s.COLUMNDATA.toString());
 
-        System.out.println(Arrays.toString(s.mean()));
+       /* System.out.println(Arrays.toString(s.mean()));
         System.out.println(Arrays.toString(s.sd()));
 
         List<float[]> sdScale = s.StandardScale();
 
-        /*for(int i = 0; i<sdScale.size(); i++){
+        *//*for(int i = 0; i<sdScale.size(); i++){
             float[] temp1 = s.calculate.get(i);
             System.out.println(Arrays.toString(temp1));
             float[] temp = sdScale.get(i);
             System.out.println(Arrays.toString(temp));
-        }*/
+        }*//*
 
         float [] sd = s.sd();
         float [] min = s.min();
@@ -71,15 +75,17 @@ public class Testing {
         System.out.println(Arrays.toString(max));
         System.out.println(Arrays.toString(range));
         System.out.println(Arrays.toString(median));
-        /*for(int i = 0; i<s.calculate.size(); i++){
+
+
+        *//*for(int i = 0; i<s.calculate.size(); i++){
             float[] temp = s.calculate.get(i);
             System.out.println(Arrays.toString(temp));
-        }*//*
+        }*//**//*
 
         System.out.println(Arrays.toString(sd));
-        *//*System.out.println(r.DATA.toString());
+        *//**//*System.out.println(r.DATA.toString());
         System.out.println(s.COLUMNDATA.toString());
-        System.out.println(Arrays.toString(mean));*//*
+        System.out.println(Arrays.toString(mean));*//**//*
 
         System.out.println(r.DATA.toString());
 
@@ -87,9 +93,10 @@ public class Testing {
 
         String [] column = {"Colour", "Yellow", "Green", "", "Black", "White", "Red"};
         c.concatColumn(column);
-        r.print();*/
+        r.print();*//*
 
         final double duration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
         System.out.println(duration);
+        */
     }
 }
