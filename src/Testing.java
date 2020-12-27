cb83faac0a600563ed3c3ad6dd76014b318869d5
Lhz0616 <lim1234172@gmail.com>
2020-12-27 23:27:09 +0800

    try

--- src/Testing.java
+++ src/Testing.java
@@ -40,10 +40,10 @@ public class Testing {
         String filePath = "C:\\Users\\user\\Desktop\\SimpleDataFrame\\src\\Book1.csv";
         final long startTime = System.nanoTime();
 
-        DBReader read = new DBReader("groupmemberdb");
+        //DBReader read = new DBReader("groupmemberdb");
         DataFrameReader r = new DataFrameReader();
-        //r.readCSV(filePath);
-        read.readDB();
+        r.readCSV(filePath);
+        //read.readDB();
         System.out.println(r.DATA.toString());
         Statistics s = new Statistics();
         System.out.println(s.COLUMNDATA.toString());
