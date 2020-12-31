1d3e943a4d9128736daa67a5c55f3ae66213dc15
Roger-wsy <73838562+Roger-wsy@users.noreply.github.com>
2020-12-31 12:37:06 +0800

    Update Drop.java

--- src/data/Drop.java
+++ src/data/Drop.java
@@ -1,53 +1,55 @@
 package data;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Drop {
-    DataFrameReader dfr = new DataFrameReader();
-    Statistics s=new Statistics();
+public class Drop{
 
     public void dropDuplicate(String[] category, int num) {
-        String[] header = dfr.DATA.get(0).toArray(new String[0]);
-        int[] a=new int[dfr.header.length];
-        int counter=0;
-        String duplicate="";
-        for (int i = 0; i < category.length; i++) {
+            String[] header = dfr.header;
+            int[] a=new int[category.length];
+            int counter=0;
+            String duplicate="";
             inner:
-            for (int j = 0; j < dfr.header.length; j++) {
-                if (category[i].equals(header[j])) {
-                    a[i]=j;
-                    break inner;
-                }
-            }
-        }
-        List<String> arr = new ArrayList<String>();
-        //for every category detected and saved in array a
-        for(int i=0;i<a.length;i++){
-            //get column and store in a list
-            for(int j=0;j<s.COLUMNDATA.size();j++) {
-                arr.add(dfr.DATA.get(j).get(a[i]));
-            }
-            //for every element in list find duplicate and remove the duplicate row based on the num
-            for(int k=0; k< arr.size(); k++) {
-                inner:
-                if (arr.lastIndexOf(arr.get(k)) != k)  {
-                    duplicate=arr.get(k);
-                    break inner;
-                }
-                x:
-                if (arr.get(k).equals(duplicate))
-                {
-                    counter++;
-                    if(counter==num){
-                        dfr.DATA.get(k).clear();
-                        break x;
+            for (int i = 0; i < category.length; i++) {
+
+                for (int j = 0; j < dfr.header.length; j++) {
+                    if (category[i].equals(header[j])) {
+                        a[i]=j;
+
                     }
                 }
             }
+            List<String> arr = new ArrayList<String>();
 
+            //for every category detected and saved in array a
+            for(int i=0;i<a.length;i++){
+                //get column and store in a list
+                for(int j=0;j<dfr.DATA.size();j++) {
+                    arr.add(dfr.DATA.get(j).get(a[i]));
+                }
+                //for every element in list find duplicate and remove the duplicate row based on the num
+                for(int k=0; k< arr.size(); k++) {
+                    inner:
+                    if (arr.lastIndexOf(arr.get(k)) != k)  {
+                        duplicate=arr.get(k);
+                        break inner;
+                    }
+
+                    if (arr.get(k).equals(duplicate))
+                    {
+                        counter++;
+                        if(counter==num){
+                            dfr.DATA.remove(k);
+                            break ;
+                        }
+                    }
+
+                }
+                arr.clear();
+                counter=1;
+            }
+
+
+            System.out.println( dfr.DATA.toString());
         }
-
-
-        System.out.println( dfr.DATA.toString());
-    }
-
-
-    }
+}
