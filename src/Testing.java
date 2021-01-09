45cf0eecbcc7d29a504dbc640c461d1f35000c9c
zhuolisam <zhuolisam0627@gmail.com>
2021-01-09 13:56:38 +0800

    updated Sort method

--- src/Testing.java
+++ src/Testing.java
@@ -1,26 +1,18 @@
 import data.DataFrameReader;
-import data.rowCol;
-import math.Statistics;
-import sql.DBReader;
-import sql.DBWriter;
-
-import java.io.IOException;
-import java.util.Arrays;
-
-import static math.Scalers.*;
+import data.Sorting;
 
 // Object
 
 public class Testing {
     public static void main(String[] args){
-        String filePath = "C:\\Users\\user\\Desktop\\SimpleDataFrame\\src\\Book1.csv";
-
-
-        //DBReader read = new DBReader("groupmemberdb", "generalinfo", "root", "");
-        DataFrameReader r = new DataFrameReader();
-        r.readCSV(filePath);
-        //read.readDB();
-        System.out.println(r);
+//        String filePath = "C:\\Users\\user\\Desktop\\SimpleDataFrame\\src\\Book1.csv";
+//
+//
+//        //DBReader read = new DBReader("groupmemberdb", "generalinfo", "root", "");
+//        DataFrameReader r = new DataFrameReader();
+//        r.readCSV(filePath);
+//        //read.readDB();
+//        System.out.println(r);
         /*Statistics s = new Statistics();
 
 
@@ -42,6 +34,13 @@ public class Testing {
         //writer.writeDB("jdbc:mysql://localhost/groupmemberdb", "newTable", "root", "");
 
 
+        String filepath = "C:\\Users\\User\\Documents\\NetBeansProjects\\DonaldProjectUpdated\\src\\Book1.csv";
+
+        DataFrameReader rd =new DataFrameReader();
+        rd.readCSV(filepath);
+
+        Sorting st = new Sorting();
+        st.sort("Age", "ascending");
 
     }
 }
--- src/data/Sorting.java
+++ src/data/Sorting.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -11,152 +12,157 @@ import java.util.List;
 public class Sorting {
 
     /**
+     * {@code index} is used to save the index the user input
+     */
+    private static int index = 0;
+    /**
      * {@code r} is used to access the dataframe
      */
     DataFrameReader r = new DataFrameReader();
 
     /**
-     * {@code index} is used to save the index the user input
-     */
-    private static int index = 0;
-
-    /**
-     * {@code sortAsc} is used to sort the selected column in ascending order
+     * {@code sort} is used to sort the data by the selected column
+     * This method will print out the sorted data
      *
-     * @param column    the column that wanted to be sort
+     * @param column the column wanted to be sort
+     * @param order in ascending order or descending order
      */
-    public void sortAsc(String column) {
+    public void sort(String column, String order) {
+        List<List<String>> SortedData = new ArrayList<>(r.DATA);    //clone the data into new List
+
         //for-loop to locate the index of column to be sorted
         for (int i = 0; i < r.header.size(); i++) {
             if (r.header.get(i).equalsIgnoreCase(column)) {
-                index = i;
+                this.index = i;
             }
         }
 
-        //After getting index, loop through the column data to check wether contain non-numeric String
-        try {
-            for (int i = 0; i < r.DATA.size(); i++) {
-                parseStringToDouble(r.DATA.get(i).get(index));
-            }
-            sortNumberAsc(r.DATA);  //if all can be parsed as Double, sort it as Double
-        } catch (NumberFormatException e) { //catch if contains non-number, and proceed to check if it is String
+        if (order.equalsIgnoreCase("ascending")) {
+            //After getting index, loop through the column data to check whether contain non-numeric String
+            try {
+                for (int i = 0; i < SortedData.size(); i++) {
+                    parseStringToFloat(SortedData.get(i).get(index));
+                }
+                sortNumberAsc(SortedData);  //if all can be parsed as Float, sort it as Float
+            } catch (NumberFormatException e) { //catch if contains non-number, and proceed to check if it is String
 
-            for (int i = 0; i < r.DATA.size(); i++) {
-                for (int j = 0; j < r.DATA.get(i).get(index).length(); j++) {
-                    char ch = r.DATA.get(i).get(index).charAt(j);
-                    if (!(Character.isLetter(ch) || ch == ' ')) {   // exit if contains not letters and spaces
-                        System.out.println("This column comprises data mixed of character and number!");    
-                        System.exit(-1);
+                for (int i = 0; i < SortedData.size(); i++) {
+                    for (int j = 0; j < SortedData.get(i).get(index).length(); j++) {
+                        char ch = SortedData.get(i).get(index).charAt(j);
+                        if (!(Character.isLetter(ch) || ch == ' ')) {   // exit if contains not letters and spaces
+                            System.out.println("This column comprises data mixed of character and number!");
+                            System.exit(-1);
+                        }
                     }
                 }
+                sortStringAsc(SortedData);  //if no other except characters and spaces, sort it as String
+
             }
-            sortStringAsc(r.DATA);  //if no other except characters and spaces, sort it as String
+
+        } else if (order.equalsIgnoreCase("Descending")) {
+            //After getting index, loop through the column data to check whether contain non-numeric String
+            try {
+                for (int i = 0; i < SortedData.size(); i++) {
+                    parseStringToFloat(SortedData.get(i).get(index));
+                }
+                sortNumberDesc(SortedData);  //if all can be parsed as Float, sort it as Float
+            } catch (NumberFormatException e) { //catch if contains non-number, and proceed to check if it is String
+                for (int i = 0; i < SortedData.size(); i++) {
+                    for (int j = 0; j < SortedData.get(i).get(index).length(); j++) {
+                        char ch = SortedData.get(i).get(index).charAt(j);
+                        if (!(Character.isLetter(ch) || ch == ' ')) {   // exit if contains not letters and spaces
+                            System.out.println("This column comprises data mixed of character and number!");
+                            System.exit(-1);
+                        }
+                    }
+                }
+                sortStringDesc(SortedData);  //if no other except characters and spaces, sort it as String
+            }
+        }else{
+            System.out.println("Please specify in ascending order or descending order.");
+            System.exit(-1);
         }
+        System.out.println(SortedData); //display the Sorted Data
     }
 
     /**
-     * {@code sortDesc} is used to sort the selected column in descending order
+     * {@code sortNumberAsc} is a method to sort the column that is numbers in ascending order
      *
-     * @param column    the column that wanted to be sort
+     * @param SortedData data to be sorted
      */
-    public void sortDesc(String column) {
-        //for-loop to locate the index of column to be sorted
-        for (int i = 0; i < DataFrameReader.header.size(); i++) {
-            if (r.header.get(i).equalsIgnoreCase(column)) {
-                index = i;
-            }
-        }
-
-        //After getting index, loop through the column data to check wether contain non-numeric String
-        try {
-            for (int i = 0; i < r.DATA.size(); i++) {
-                parseStringToDouble(r.DATA.get(i).get(index));
-            }
-            sortNumberDesc(r.DATA); //if all can be parsed as Double, sort it as Double
-        } catch (NumberFormatException e) { //catch if contains non-number, and proceed to check if it is String
-            for (int i = 0; i < r.DATA.size(); i++) {
-                for (int j = 0; j < r.DATA.get(i).get(index).length(); j++) {
-
-                    char ch = r.DATA.get(i).get(index).charAt(j);
-
-                    if (!(Character.isLetter(ch) || ch == ' ')) {
-                        System.out.println("This column comprises data mixed of character and number!");    // exit if contains not letters and spaces
-                        System.exit(-1);
-                    }
-                }
-            }
-            sortStringDesc(r.DATA); //if no other except characters and spaces, sort it as String
-        }
-    }
-
-    /**
-     * {@code sortNumberAsc} is a method to sort the column that have
-     * @param DATA
-     * @return
-     */
-    public List<List<String>> sortNumberAsc(List<List<String>> DATA) {
+    public void sortNumberAsc(List<List<String>> SortedData) {
         //create a customized comparator that compare Double value parsed from String
         Comparator<List<String>> comparator = new Comparator<List<String>>() {
-
             @Override
             //ascending comparison
             public int compare(List<String> object1, List<String> object2) {
-                return Double.compare(parseStringToDouble(object1.get(index)), parseStringToDouble(object2.get(index)));
+                return Double.compare(parseStringToFloat(object1.get(index)), parseStringToFloat(object2.get(index)));
             }
-
         };
-        //sort DATA with the customized comparator
-        Collections.sort(DATA, comparator);
-        return DATA;
+        //sort SortedData with the customized comparator
+        Collections.sort(SortedData, comparator);
     }
 
-    public List<List<String>> sortNumberDesc(List<List<String>> DATA) {
-        //create a customized comparator that compare Double value parsed from String 
+    /**
+     * {@code sortNumberDesc} is a method to sort the column that is numbers in descending order
+     *
+     * @param SortedData data to be sorted
+     */
+    public void sortNumberDesc(List<List<String>> SortedData) {
+        //create a customized comparator that compare Double value parsed from String
         Comparator<List<String>> comparator = new Comparator<List<String>>() {
-
             @Override
-            //desending comparison
+            //descending comparison
             public int compare(List<String> object1, List<String> object2) {
-                return -1 * Double.compare(parseStringToDouble(object1.get(index)), parseStringToDouble(object2.get(index)));
+                return -1 * Double.compare(parseStringToFloat(object1.get(index)), parseStringToFloat(object2.get(index)));
             }
-
         };
-        //sort DATA with the customized comparator
-        Collections.sort(DATA, comparator);
-        return DATA;
+        //sort SortedData with the customized comparator
+        Collections.sort(SortedData, comparator);
     }
 
-    public List<List<String>> sortStringAsc(List<List<String>> DATA) {
+
+    /**
+     * {@code sortStringrAsc} is a method to sort the column that is letters in ascending order
+     *
+     * @param SortedData data to be sorted
+     */
+    public void sortStringAsc(List<List<String>> SortedData) {
         //create a customized comparator that compare String
         Comparator<List<String>> comparator = new Comparator<List<String>>() {
-
             @Override
             public int compare(List<String> object1, List<String> object2) {
                 return object1.get(index).compareTo(object2.get(index));
             }
         };
         //sort DATA with the customized comparator
-        Collections.sort(DATA, comparator);
-        return DATA;
+        Collections.sort(SortedData, comparator);
     }
 
-    public List<List<String>> sortStringDesc(List<List<String>> DATA) {
+    /**
+     * {@code sortStringrDesc} is a method to sort the column that is letters in descending order
+     *
+     * @param SortedData data to be sorted
+     */
+    public void sortStringDesc(List<List<String>> SortedData) {
         //create a customized comparator that compare String
         Comparator<List<String>> comparator = new Comparator<List<String>>() {
-
             @Override
             public int compare(List<String> object1, List<String> object2) {
                 return -1 * object1.get(index).compareTo(object2.get(index));
             }
         };
         //sort DATA with the customized comparator
-        Collections.sort(DATA, comparator);
-        return DATA;
+        Collections.sort(SortedData, comparator);
     }
 
+    /**
+     * {@code parseStringToFloat} is a method to sort the column that is letters in descending order
+     *This method will parse 0 when a empty data of number type is found, so that empty data will not be an error
+     * @param value that is being parsed
+     */
     //parse 0 if empty String is found
-    public double parseStringToDouble(String value) {
-        return value.isEmpty() || value == "" ? 0.0 : Double.parseDouble(value);
+    public double parseStringToFloat(String value) {
+        return value.isEmpty() || value.equals("") ? 0.0 : Float.parseFloat(value);
     }
-
 }
